3f304a234d4ad438ccc42165595500e8bd7555a7
SaiSiddhartha97 <43650757+SaiSiddhartha97@users.noreply.github.com>
2019-04-10 16:30:19 +0530

    Add files via upload

---
+++ Theme_based/register.js
@@ -0,0 +1,32 @@
+document.getElementById('register').addEventListener('click',function sid() {
+	// body...
+	document.querySelector(".bg-modal").style.display='flex';
+});
+
+document.querySelector('.close2').addEventListener('click',function sid1(){
+	// body...
+	document.querySelector('.bg-modal').style.display='none';
+});
+
+
+document.getElementById('sign-in').addEventListener('click',function sid2() {
+	// body...
+	document.querySelector(".bg-model").style.display='flex';
+});
+
+document.querySelector('.close1').addEventListener('click',function sid3(){
+	// body...
+	document.querySelector('.bg-model').style.display='none';
+});
+
+document.getElementById('forgot-password').addEventListener('click',function sid4() {
+	// body...
+	document.querySelector('.bg-model').style.display='none';
+	document.querySelector(".bg-modall").style.display='flex';
+});
+
+
+document.querySelector('.close3').addEventListener('click',function sid5(){
+	// body...
+	document.querySelector('.bg-modall').style.display='none';
+});
